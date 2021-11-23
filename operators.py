d8cfbd981a4175fbb29c42fd7b12c66b7f10a2eb
ysj1173886760 <1173886760@qq.com>
2021-11-23 15:39:40 +0800

    initial commit

---
+++ operators.py
@@ -0,0 +1,230 @@
+# coding=utf-8
+import numpy as np
+import struct
+import os
+import time
+
+class FullyConnectedLayer(object):
+    def __init__(self, num_input, num_output):  # 全连接层初始化
+        self.num_input = num_input
+        self.num_output = num_output
+        print('\tFully connected layer with input %d, output %d.' % (self.num_input, self.num_output))
+
+    def init_param(self, std=0.01):  # 参数初始化
+        self.weight = np.random.normal(loc=0.0, scale=std, size=(self.num_input, self.num_output))
+        self.bias = np.zeros([1, self.num_output])
+
+    def forward(self, input):  # 前向传播计算
+        self.input = input
+        self.output = np.matmul(input, self.weight) + self.bias
+        return self.output
+
+    def backward(self, top_diff):  # 反向传播的计算
+        self.d_weight = np.matmul(self.input.T, top_diff)
+        self.d_bias = top_diff.T
+        bottom_diff = np.matmul(top_diff, self.weight.T)
+        return bottom_diff
+
+    def update_param(self, lr):  # 参数更新
+        self.weight = self.weight - lr * self.d_weight
+        self.bias = self.bias - lr * np.sum(self.d_bias, axis=1)
+
+    def load_param(self, weight, bias):  # 参数加载
+        assert self.weight.shape == weight.shape, "{} {}".format(self.weight.shape, weight.shape)
+        assert self.bias.shape == bias.shape
+        self.weight = weight
+        self.bias = bias
+
+    def save_param(self):  # 参数保存
+        return self.weight, self.bias
+
+class ReLULayer(object):
+    def __init__(self):
+        print('\tReLU layer.')
+
+    def forward(self, input):  # 前向传播的计算
+        self.input = input
+        output = (input > 0) * input
+        return output
+
+    def backward(self, top_diff):  # 反向传播的计算
+        bottom_diff = (self.input > 0) * top_diff
+        return bottom_diff
+
+class SoftmaxLossLayer(object):
+    def __init__(self):
+        print('\tSoftmax loss layer.')
+
+    def forward(self, input):  # 前向传播的计算
+        input_max = np.max(input, axis=1, keepdims=True)
+        input_exp = np.exp(input - input_max)
+        self.prob = input_exp / np.sum(input_exp, axis=1, keepdims=True)
+        return self.prob
+
+    def get_loss(self, label):   # 计算损失
+        self.batch_size = self.prob.shape[0]
+        self.label_onehot = np.zeros_like(self.prob)
+        self.label_onehot[np.arange(self.batch_size), label] = 1.0
+        loss = -np.sum(np.log(self.prob) * self.label_onehot) / self.batch_size
+        return loss
+
+    def backward(self):  # 反向传播的计算
+        # TODO：softmax 损失层的反向传播，计算本层损失
+        bottom_diff = (self.prob - self.label_onehot) / self.batch_size
+        return bottom_diff
+
+class ConvolutionalLayer(object):
+    def __init__(self, kernel_size, channel_in, channel_out, padding, stride):
+        self.kernel_size = kernel_size
+        self.channel_in = channel_in
+        self.channel_out = channel_out
+        self.padding = padding
+        self.stride = stride
+        print('\tConvolutional layer with kernel size %d, input channel %d, output channel %d.' % (self.kernel_size, self.channel_in, self.channel_out))
+
+    def init_param(self, std=0.01):
+        self.weight = np.random.normal(loc=0.0, scale=std, size=(self.channel_in, self.kernel_size, self.kernel_size, self.channel_out))
+        self.bias = np.zeros([self.channel_out])
+
+    def forward(self, input):
+        # TODO: 改进forward函数，使得计算加速
+        self.input = input # [N, C, H, W]
+        height = input.shape[2] + 2 * self.padding
+        width = input.shape[3] + 2 * self.padding
+        self.input_pad = np.zeros([self.input.shape[0], self.input.shape[1], height, width])
+        self.input_pad[:, :, self.padding: self.padding + input.shape[2], self.padding: self.padding + input.shape[3]] = self.input
+        height_out = (height - self.kernel_size) / self.stride + 1
+        width_out = (width - self.kernel_size) / self.stride + 1
+        mat_w = self.kernel_size * self.kernel_size * self.channel_in
+        mat_h = height_out * width_out
+
+        self.col = np.empty((input.shape[0], mat_h, mat_w))
+        cur = 0
+        for x in range(height_out):
+            for y in range(width_out):
+                bias_x = x * self.stride
+                bias_y = y * self.stride
+                self.col[:, cur, :] = self.input_pad[:, :, bias_x: bias_x + self.kernel_size, bias_y: bias_y + self.kernel_size].reshape(input.shape[0], -1)
+                cur = cur + 1
+        output = np.matmul(self.col, self.weight.reshape(-1, self.weight.shape[-1])) + self.bias
+        self.output = np.moveaxis(output.reshape(input.shape[0], height_out, width_out, self.channel_out), 3, 1)
+
+        return self.output
+
+    def backward(self, top_diff):
+        # top_diff batch, cout, h, w
+
+        height_out = (self.input.shape[2] + 2 * self.padding - self.kernel_size) / self.stride + 1
+        width_out = (self.input.shape[3] + 2 * self.padding - self.kernel_size) / self.stride + 1
+
+        # cout, batch, h, w
+        top_diff_col = np.transpose(top_diff, [1, 0, 2, 3]).reshape(top_diff.shape[1], -1)
+        # self.col batch, (h * w), (cin * k * k)
+
+        # what we want, cin, k, k, cout
+        tmp = np.transpose(self.col.reshape(-1, self.col.shape[-1]), [1, 0])
+        self.d_weight = np.matmul(tmp, top_diff_col.T).reshape(self.channel_in, self.kernel_size, self.kernel_size, self.channel_out)
+        self.d_bias = top_diff_col.sum(axis=1)
+        
+        backward_col = np.empty((top_diff.shape[0], self.input.shape[2] * self.input.shape[3], self.kernel_size * self.kernel_size * self.channel_out))
+        pad_height = ((self.input.shape[2] - 1) * self.stride + self.kernel_size - height_out) / 2
+        pad_width = ((self.input.shape[3] - 1) * self.stride + self.kernel_size - width_out) / 2
+        top_diff_pad = np.zeros((top_diff.shape[0], top_diff.shape[1], height_out + 2 * pad_height, width_out + 2 * pad_width))
+        top_diff_pad[:, :, pad_height: height_out + pad_height, pad_width: width_out + pad_width] = top_diff
+        cur = 0
+        for x in range(self.input.shape[2]):
+            for y in range(self.input.shape[3]):
+                bias_x = x * self.stride
+                bias_y = y * self.stride
+                backward_col[:, cur, :] = top_diff_pad[:, :, bias_x: bias_x + self.kernel_size, bias_y: bias_y + self.kernel_size].reshape(top_diff.shape[0], -1)
+                cur = cur + 1
+
+        # backward_col [batch, height * width, cout * k * k]
+        # try to draw a draft and you will know the reason.
+        # you shall consider the contribution from top_diff to the original dx
+        # if x * kernel[i] has contribution to y, then dy * kernel[size - i] will have contribution
+        weight_tmp = np.transpose(self.weight, [3, 1, 2, 0]).reshape(self.channel_out, -1, self.channel_in)[:, ::-1, :].reshape(-1, self.channel_in)
+        bottom_diff = np.matmul(backward_col, weight_tmp)
+        # [batch, height, width, cin]
+        bottom_diff = np.transpose(bottom_diff.reshape(top_diff.shape[0], self.input.shape[2], self.input.shape[3], self.input.shape[1]), [0, 3, 1, 2])
+        
+        return bottom_diff
+
+    def get_gradient(self):
+        return self.d_weight, self.d_bias
+
+    def update_param(self, lr):
+        self.weight += - lr * self.d_weight
+        self.bias += - lr * self.d_bias
+
+    def load_param(self, weight, bias):
+        assert self.weight.shape == weight.shape
+        assert self.bias.shape == bias.shape
+        self.weight = weight
+        self.bias = bias
+
+class MaxPoolingLayer(object):
+    def __init__(self, kernel_size, stride, type=1):
+        self.kernel_size = kernel_size
+        self.stride = stride
+        print('\tMax pooling layer with kernel size %d, stride %d.' % (self.kernel_size, self.stride))
+
+    def forward(self, input):
+        # TODO: 改进forward函数，使得计算加速
+        self.input = input # [N, C, H, W]
+        height_out = (self.input.shape[2] - self.kernel_size) / self.stride + 1
+        width_out = (self.input.shape[3] - self.kernel_size) / self.stride + 1
+        mat_w = self.kernel_size * self.kernel_size
+        mat_h = height_out * width_out
+
+        col = np.empty((input.shape[0], self.input.shape[1], mat_h, mat_w))
+        cur = 0
+        for x in range(height_out):
+            for y in range(width_out):
+                bias_x = x * self.stride
+                bias_y = y * self.stride
+                col[:, :, cur, :] = self.input[:, :, bias_x: bias_x + self.kernel_size, bias_y: bias_y + self.kernel_size].reshape(input.shape[0], input.shape[1], -1)
+                cur = cur + 1
+
+        self.output = np.max(col, axis=3, keepdims=True)
+        max_index = np.argmax(col.reshape(input.shape[0], input.shape[1], height_out, width_out, self.kernel_size * self.kernel_size), axis=4)
+        self.max_elements = np.zeros((input.shape[0], self.input.shape[1], height_out, width_out, self.kernel_size * self.kernel_size))
+        # https://stackoverflow.com/questions/44143438/numpy-indexing-set-1-to-max-value-and-zeros-to-all-others
+        # refering the advanced indexing in numpy
+        n, c, h, w = self.max_elements.shape[: 4]
+        N, C, H, W = np.ogrid[:n, :c, :h, :w]
+        self.max_elements[N, C, H, W, max_index] = 1
+        self.output = self.output.reshape(input.shape[0], input.shape[1], height_out, width_out)
+        return self.output
+
+    def backward(self, top_diff):
+        # TODO: 改进backward函数，使得计算加速
+        bottom_diff = np.zeros(self.input.shape)
+        contrib = self.max_elements * (top_diff.reshape(list(top_diff.shape) + [1]))
+        for x in range(top_diff.shape[2]):
+            for y in range(top_diff.shape[3]):
+                bias_x = x * self.stride
+                bias_y = y * self.stride
+                bottom_diff[:, :, bias_x: bias_x + self.kernel_size, bias_y: bias_y + self.kernel_size] += contrib[:, :, x, y, :].reshape(top_diff.shape[0], top_diff.shape[1], self.kernel_size, self.kernel_size)
+        return bottom_diff
+
+class FlattenLayer(object):
+    def __init__(self, input_shape, output_shape):
+        self.input_shape = input_shape
+        self.output_shape = output_shape
+        assert np.prod(self.input_shape) == np.prod(self.output_shape)
+        print('\tFlatten layer with input shape %s, output shape %s.' % (str(self.input_shape), str(self.output_shape)))
+
+    def forward(self, input):
+        assert list(input.shape[1:]) == list(self.input_shape)
+        # matconvnet feature map dim: [N, height, width, channel]
+        # ours feature map dim: [N, channel, height, width]
+        self.input = np.transpose(input, [0, 2, 3, 1])
+        self.output = self.input.reshape([self.input.shape[0]] + list(self.output_shape))
+        return self.output
+
+    def backward(self, top_diff):
+        assert list(top_diff.shape[1:]) == list(self.output_shape)
+        top_diff = np.transpose(top_diff, [0, 3, 1, 2])
+        bottom_diff = top_diff.reshape([top_diff.shape[0]] + list(self.input_shape))
+        return bottom_diff
